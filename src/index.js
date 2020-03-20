41c99d08cf36b3da3d4dec359b3d2ea8a57bd363
anbunathan <anbunathan.r@gmail.com>
2020-03-20 15:18:07 +0530

    Tutorial5 commit:Setup redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import decode from "jwt-decode";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "semantic-ui-css/semantic.min.css";
 
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>, 
+    <BrowserRouter>
+        <Provider>
+            <App />
+        </Provider>    
+    </BrowserRouter>, 
 document.getElementById('root'));
 
 
