1bed1554d7209a500da53f89de2432286e980b26
anbunathan <anbunathan.r@gmail.com>
2020-03-20 14:37:33 +0530

    Tutorial4 commit:Implement login form, Inline Error

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
 class LoginPage extends React.Component {
+    submit = data => {
+        console.log(data);
+    };
     render() {
         return (
             <div>
                 <h1>Login page</h1>
+
+                <LoginForm submit={this.submit}/>
             </div>
         );
     }
