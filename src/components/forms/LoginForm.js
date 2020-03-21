30f1b32bf5512b7e133da8805e96ab7dcd31cb3b
anbunathan <anbunathan.r@gmail.com>
2020-03-21 11:12:23 +0530

    Tutorial7 commit:render server validation error

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -22,11 +22,11 @@ class LoginForm extends React.Component {
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
-    if (Object.keys(errors).length === 0) {
-      this.setState({ loading: true });
+    if (Object.keys(errors).length === 0) {  
+      this.setState({loading: true});    
       this.props
         .submit(this.state.data)
-        ;
+        .catch(err => this.setState({errors: err.response.data.errors, loading: false}));
     }
   };
 
