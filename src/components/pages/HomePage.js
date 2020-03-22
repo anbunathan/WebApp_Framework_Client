087d2169a65f0c8551c9db9826cc0daf163f377a
anbunathan <anbunathan.r@gmail.com>
2020-03-22 19:28:46 +0530

    Tutorial11 commit: Client side User Signs Up

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,13 +5,17 @@ import { connect } from "react-redux";
 import * as actions from "../../actions/auth";
 
 const HomePage = ({ isAuthenticated, logout }) => (
-    <div>
-      <h1>Home Page</h1>
-      {isAuthenticated?
-      <button onClick={() => logout()}>Logout</button>:
-      <Link to ="/login">Login</Link>}
-    </div>
-  );
+  <div>
+    <h1>Home Page</h1>
+    {isAuthenticated ? (
+      <button onClick={() => logout()}>Logout</button>
+    ) : (
+      <div>
+        <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link>
+      </div>
+    )}
+  </div>
+);
   
 HomePage.propTypes = {
     isAuthenticated: PropTypes.bool.isRequired,
